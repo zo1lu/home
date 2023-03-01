7afd5e75ba2b7ef0c47165610d83c612809cc402
Meng-Chin Lu <98194822+zo1lu@users.noreply.github.com>
2023-03-01 17:28:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+let formSection = document.getElementById("form-section");
+let src = "https://zo1lu.github.io/home/data.json";
+fetch(src).then(function(response){
+  return response.json()
+}).then(function(result){
+  console.log("data:",result);
+  let data
+  for(i=0;i<result.length;i++){
+    const question = data[i].question;
+    const answer = data[i].answer;
+    const div = document.createElement("div");
+    div.className = "form";
+    const h3 = document.createElement("h3");
+    h3.className = "question";
+    const p = document.createElement("p");
+    p.className = "answer";
+    h3.innerText = question;
+    p.innerText = answer;
+    
+    div.appendChild(h3);
+    div.appendChild(p);
+    formSection.appendChild(div);
+  }
+})
